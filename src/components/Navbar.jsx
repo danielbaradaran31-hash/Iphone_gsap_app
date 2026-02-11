95715ec555db7d84bf7a13e7825bd690d8a80f6a
daniel <danielbaradaran31-hash>
2026-02-11 12:44:43 +0100

    add icon navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,21 @@ const Navbar = () => {
                     { label: 'iPhone' },
                     { label: 'Vision' },
                     { label: 'AirPods' },
-                ]}
+                ].map(({label}) => (
+                    <li key={label}>
+                        <a href={label}>{label}</a>
+                    </li>
+                ))}
             </ul>
+
+            <div className='flex-center gap-3'>
+                <button>
+                    <img src='/search.svg' alt='Search' />
+                </button>
+                <button>
+                    <img src='/cart.svg' alt='Cart' />
+                </button>
+            </div>
         </nav>
 
     </header>
